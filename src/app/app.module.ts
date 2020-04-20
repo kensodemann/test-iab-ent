ec8a36d7417ed220e3f94b4f7d4186a5bd30597a
Ken Sodemann <ken@ionic.io>
2020-04-20 11:50:40 -0500

    add the enterprise plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-enterprise/inappbrowser/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -13,11 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
+  providers: [InAppBrowser, StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-enterprise/inappbrowser/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
-
+  open(target?: string) {
+    this.iab.create('https://www.google.com', target).show();
+  }
 }
